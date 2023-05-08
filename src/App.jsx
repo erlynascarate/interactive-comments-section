8926f501aac676964e6718b0c218b5ea9410b03e
Erlyn Ascarate <erlyndavid@outlook.es>
2023-05-08 15:41:59 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import {
+    createTheme,
+    CssBaseline,
+    responsiveFontSizes,
+    ThemeProvider,
+} from '@mui/material'
+
+let theme = createTheme({
+    typography: {
+        fontFamily: 'Rubik, sans-serif',
+    },
+    palette: {
+        primary: {
+            main: 'hsl(238, 40%, 52%)',
+        },
+        warning: {
+            main: 'hsl(358, 79%, 66%)',
+        },
+        text: {
+            primary: 'hsl(239, 57%, 85%)',
+            secondary: 'hsl(212, 24%, 26%)',
+        },
+        background: {
+            default: 'hsl(228, 33%, 97%)',
+        },
+    },
+})
+
+theme = responsiveFontSizes(theme)
+
+function App() {
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+        </ThemeProvider>
+    )
+}
+
+export default App
