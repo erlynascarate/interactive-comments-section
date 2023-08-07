0de3907ec26f7917bdfa4c92bd5b7c560bbf1a95
Erlyn Ascarate <erlynascarate@gmail.com>
2023-08-07 15:28:48 -0500

    Score buttons work

--- src/components/ToggleButtons.jsx
+++ src/components/ToggleButtons.jsx
@@ -1,13 +1,30 @@
 import { useState } from 'react'
-import { ToggleButton, ToggleButtonGroup, Typography } from '@mui/material'
+import { ToggleButton, ToggleButtonGroup } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add'
 import RemoveIcon from '@mui/icons-material/Remove'
 
-const ToggleButtons = ({ score }) => {
-    const [alignment, setAlignment] = useState(null)
+const ToggleButtons = (props) => {
+    const { score } = props
 
-    const handleAlignment = (event, newAlignment) => {
+    const [alignment, setAlignment] = useState(null)
+    const [number, setNumber] = useState(score)
+
+    const handleAlignment = (_event, newAlignment) => {
         setAlignment(newAlignment)
+
+        switch (newAlignment) {
+            case 'plus':
+                setNumber(score + 1)
+                break
+
+            case 'minus':
+                setNumber(score - 1)
+                break
+
+            default:
+                setNumber(score)
+                break
+        }
     }
 
     return (
@@ -23,30 +40,37 @@ const ToggleButtons = ({ score }) => {
             <ToggleButton
                 sx={{
                     border: 'none',
-                    color: 'hsl(239, 57%, 85%)',
+                    color: 'primary.hover',
                 }}
                 value='plus'
             >
                 <AddIcon fontSize='inherit' />
             </ToggleButton>
-            <Typography
-                sx={{
-                    height: 36,
-                    width: 36,
-                    fontWeight: 700,
-                    textAlign: 'center',
-                    lineHeight: 2.5,
-                }}
-                color='primary'
-                component='span'
-            >
-                {score}
-            </Typography>
             <ToggleButton
                 sx={{
-                    border: 'none',
-                    color: 'hsl(239, 57%, 85%)',
+                    border: 'none !important',
+                    width: 36,
+                    fontWeight: 700,
+                    lineHeight: 1,
+                    '&.Mui-disabled': {
+                        color: 'primary.main',
+                    },
                 }}
+                disabled
+                value='score'
+            >
+                {number}
+            </ToggleButton>
+            <ToggleButton
+                sx={{
+                    border: 'none !important',
+                    color: 'primary.hover',
+                    '&.Mui-disabled': {
+                        pointerEvents: 'initial',
+                        cursor: 'not-allowed',
+                    },
+                }}
+                disabled={score === 0 && true}
                 value='minus'
             >
                 <RemoveIcon fontSize='inherit' />
